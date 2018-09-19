00ed326f5a8dc7fdc911806f364edf75a156247a
Kevin Crew <kcrew@advogroup.co.uk>
2018-09-19 14:04:09 +0100

    Product managament pages completed and tested

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepostory.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepostory
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        // This is the constructor 
+        public ProductRepostory()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>(); 
+            }
+        }
+
+        // this is the method 
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+
+        // this is the add 
+        public void Insert(Product product)
+        {
+            products.Add(product);
+        }
+
+        // update method takes a product 
+        public void Update(Product product)
+        {
+            //create a new object of type product and set it to the product that we find in the products list 
+            // this uses a lamda expressein 
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        // find the item 
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found!");
+            }
+
+        }
+
+        // return a list that can be queried 
+    public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete (string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id);
+            
+            if(productToDelete != null){
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found !");
+            }
+
+        }
+    }
+}
---
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepostory context;
+
+        public ProductManagerController() {
+            context = new ProductRepostory();
+        }
+
+        // GET: ProductManager and displays a list 
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList(); 
+
+            return View(products);
+        }
+
+        // Create the add page 
+        // this first part calls the create page with an empty form 
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        // This creates the add page after it has been submited 
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            // check to see if the validation has been met and if not redirect to the add page 
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        // Create the edit 
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        //Create the actual save of the edit 
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product productToEdit = context.Find(Id);
+            if(productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid) {
+                    return View(product);
+                }
+                else
+                {
+                    productToEdit.Category = product.Category;
+                    productToEdit.Description = product.Description;
+                    productToEdit.Image = product.Image;
+                    productToEdit.Name = product.Name;
+                    productToEdit.Price = product.Price;
+
+                    context.Commit();
+
+                    return RedirectToAction("Index");
+                } 
+
+                    
+            }
+
+
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+
+            if(productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+
+            
+        }
+
+    }
+}
