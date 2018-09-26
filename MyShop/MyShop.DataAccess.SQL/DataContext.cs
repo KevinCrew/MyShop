2ac208bb41dd505facb0e701ed36a9fb35470007
Kevin Crew <kcrew@advogroup.co.uk>
2018-09-26 11:40:10 +0100

    Apply Entity Framework

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        // We create a contructor that accesses the connection string in the Web Config 
+        public DataContext() 
+            : base("DefaultConnection")
+        {
+        }
+
+        // next we have to tell the dbcontext which models we want to create the database tables 
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
