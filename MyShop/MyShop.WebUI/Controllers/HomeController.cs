e7adc292ab1815b61f689d82050b30d8fb34094f
Kevin Crew <kcrew@advogroup.co.uk>
2018-09-28 10:39:29 +0100

    Added product Listings for the user

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,39 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+
+        // The contexr is now using the Irepository 
+        IRepository<Product> context;
+        // this is added to get the product categoires from from database 
+        IRepository<ProductCategory> productCategories;
+
+        // this is the dependancy injection of the irepositorys 
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+
+            return View(products);
         }
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+                if (product == null)
+            {
+                return HttpNotFound();
+            }
+                else
+            {
+                return View(product);
+            }
+            
+        }
+        
 
         public ActionResult About()
         {
